70144cbd65d538659fba5a5c22123ba324e808d5
chineducode <chinedunnaemeka2@gmail.com>
2024-02-07 11:47:54 +0100

    header.jsx modified

---
+++ components/Avatar.jsx
@@ -0,0 +1,9 @@
+import { FaUser } from 'react-icons/fa6'
+
+export default function Avatar(){
+    return(
+        <div className="avatar">
+            <FaUser />
+        </div>
+    )
+}
--- components/Header.jsx
+++ components/Header.jsx
@@ -1,4 +1,7 @@
-import { FaBars } from 'react-icons/fa6'
+import { FaBars, FaCartShopping } from 'react-icons/fa6'
+import Link from 'next/link'
+import Nav from './Nav'
+import Avatar from './Avatar'
 
 export default function Header(){
     return(
@@ -6,8 +9,14 @@ export default function Header(){
             <div className="hamburger-logo-nav">
                 <div className="hamburger-logo">
                     <FaBars />
-                    <div className="logo"> <img src="/logo.svg" alt="logo" /> </div>
+                    <Link href={'/'} className="logo"> <img src="/logo.svg" alt="logo" /> </Link>
                 </div>
+                <Nav />
+            </div>
+
+            <div className="cart-avatar">
+                <FaCartShopping />
+                <Avatar />
             </div>
         </header>
     )
