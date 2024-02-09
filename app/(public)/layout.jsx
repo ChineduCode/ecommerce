88b1ffcef70b7734e8be78ecd4bc7c5a0462d00a
chineducode <chinedunnaemeka2@gmail.com>
2024-02-09 14:28:01 +0100

    header.jsx modified

--- app/(public)/layout.jsx
+++ app/(public)/layout.jsx
@@ -1,5 +1,6 @@
 import Header from "@/components/Header"
 import Footer from "@/components/Footer"
+import '@/css/public/main.css'
 
 export default function PublicLayout({children}){
     return(
--- components/Header.jsx
+++ components/Header.jsx
@@ -8,14 +8,17 @@ export default function Header(){
         <header>
             <div className="hamburger-logo-nav">
                 <div className="hamburger-logo">
-                    <FaBars />
+                    <FaBars size={22}/>
                     <Link href={'/'} className="logo"> <img src="/logo.svg" alt="logo" /> </Link>
                 </div>
                 <Nav />
             </div>
 
             <div className="cart-avatar">
-                <FaCartShopping />
+                <div className="cart-container">
+                    <FaCartShopping  size={20}/>
+                    <small className="cart-number-of-items">3</small>
+                </div>
                 <Avatar />
             </div>
         </header>
