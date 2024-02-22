f2395641256e5e3ab2ecb56c6bdf32bd4b3d7cf8
chineducode <chinedunnaemeka2@gmail.com>
2024-02-22 01:59:20 +0100

    hompage updated

--- components/HomePage.jsx
+++ components/HomePage.jsx
@@ -1,26 +1,25 @@
-//import Products from "./Products"
+import Products from "./Products"
 
-// const fetchProducts = async ()=> {
-//     const apiUrl = process.env.api_url
-//     const res = await fetch(`${apiUrl}/api/products`, { next: { revalidate: 10 } })
-//     const data = await res.json()
-//     return data;
-// }
+const fetchProducts = async ()=> {
+    const apiUrl = process.env.api_url
+    const res = await fetch(`${apiUrl}/api/products`, { next: { revalidate: 10 } })
+    const data = await res.json()
+    return data;
+}
 
 export default async function HomePage(){
-    //const latestProducts = await fetchProducts()
+    const latestProducts = await fetchProducts()
 
     return(
         <main className="home-page">
-            {/* <section className="latest-products">
+            <section className="latest-products">
                 <h1 className="heading">Latest Products</h1>
                 <div className="products">
                     { latestProducts.map((product, index)=>
                         <Products product={product} key={index} />
                     )}
                 </div>
-            </section> */}
-            hello world
+            </section>
         </main>
     )
 }
