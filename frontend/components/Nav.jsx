4ba3efcade34b09796d2997ec16d0c65f66146d7
chineducode <chinedunnaemeka2@gmail.com>
2024-08-21 19:11:00 +0100

    sub-nav work on

--- frontend/components/Nav.jsx
+++ frontend/components/Nav.jsx
@@ -1,24 +1,25 @@
 import { IoClose } from "react-icons/io5";
 import { FaAngleDown, FaAngleUp } from "react-icons/fa";
 import Link from "next/link";
-import { useEffect } from "react";
+import { useState } from "react";
 
 export default function Nav({ navActive, setNavActive }){
-    useEffect(()=> {
-        console.log(navActive)
-    },[navActive])
+    const [ subNavActive, setSubNavActive ] = useState(false)
+
     return(
         <nav className={`nav ${navActive ? 'nav-active' : 'nav'}`}>
             <div className="close-nav" onClick={()=> setNavActive(false)}> <IoClose size={25}/> </div>
             <ul className="nav-list">
                 <li className="link"> <Link href='/'>Home</Link> </li>
                 <ul className="link">
-                    <div className='shop-link'> <span>Shop</span> <FaAngleDown /> <FaAngleUp /> </div>
-                    <div className="shop-link-mobile">
-                        <li> <Link href='/shop/men'>Men</Link> </li>
-                        <li> <Link href='/shop/women'>Women</Link> </li>
-                        <li> <Link href='/shop/watches'>Watches</Link> </li>
-                        <li> <Link href='/shop/footwears'>Footwears</Link> </li>
+                    <div className='shop-link' onClick={()=> setSubNavActive(!subNavActive)}>
+                        <span>Shop</span> { !subNavActive && <FaAngleDown /> } { subNavActive && <FaAngleUp /> }
+                    </div>
+                    <div className={`shop-link-mobile ${subNavActive ? 'shop-mobile-active' : 'shop-link-mobile'}`}>
+                        <li className="link"> <Link href='/shop/men'>Men</Link> </li>
+                        <li className="link"> <Link href='/shop/women'>Women</Link> </li>
+                        <li className="link"> <Link href='/shop/watches'>Watches</Link> </li>
+                        <li className="link"> <Link href='/shop/footwears'>Footwears</Link> </li>
                     </div>
                 </ul>
                 <li className="link"> <Link href='/our-story'>Our Story</Link> </li>
