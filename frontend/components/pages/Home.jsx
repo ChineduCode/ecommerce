5f56fcf61e7fcb76d4b0b4bd705f5cc2d35dd28b
chineducode <chinedunnaemeka2@gmail.com>
2024-08-27 00:41:59 +0100

    images for the products added

--- frontend/components/pages/Home.jsx
+++ frontend/components/pages/Home.jsx
@@ -1,5 +1,22 @@
+import Link from "next/link"
+import { FaArrowRightLong } from "react-icons/fa6";
+
 export default function Home(){
     return(
-        <main className="homepage">Welcome to my homepage</main>
+        <main className="homepage">
+            <div className="hero-section">
+                <div className="img-container">
+                    <img src="/web-images/hero-img.jpg" alt="" />
+                </div>
+                <div className="text-container">
+                    <div className="header">Classic Exclusive</div>
+                    <h2 className="collection">Unisex Collection</h2>
+                    <div className="discount">UPTO 40% OFF</div>
+                    <button className="cta-btn">
+                        <Link href='/shop'><span>Shop Now</span> <FaArrowRightLong /></Link>
+                    </button>
+                </div>
+            </div>
+        </main>
     )
 }
