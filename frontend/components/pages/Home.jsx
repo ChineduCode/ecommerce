5d1757e740c9d11dbb17c2d8fcac177413be8b4e
chineducode <chinedunnaemeka2@gmail.com>
2024-08-27 20:06:22 +0100

    hero-section added to the homepage

--- frontend/components/pages/Home.jsx
+++ frontend/components/pages/Home.jsx
@@ -4,9 +4,10 @@ import { FaArrowRightLong } from "react-icons/fa6";
 export default function Home(){
     return(
         <main className="homepage">
-            <div className="hero-section">
+            <section className="hero-section">
                 <div className="img-container">
-                    <img src="/web-images/hero-img.jpg" alt="" />
+                    {/* <img src="/web-images/hero-img.jpg" alt="hero-img" className='mobile-img'/> */}
+                    <img src="/web-images/hero-img2.png" alt="heri-img" />
                 </div>
                 <div className="text-container">
                     <div className="header">Classic Exclusive</div>
@@ -16,7 +17,7 @@ export default function Home(){
                         <Link href='/shop'><span>Shop Now</span> <FaArrowRightLong /></Link>
                     </button>
                 </div>
-            </div>
+            </section>
         </main>
     )
 }
