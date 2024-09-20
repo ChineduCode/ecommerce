2003d8b2d835dedd7de190200ee86b3114853e46
chineducode <chinedunnaemeka2@gmail.com>
2024-09-20 15:30:03 +0100

    alot of things happened

--- frontend/app/login/page.jsx
+++ frontend/app/login/page.jsx
@@ -2,8 +2,6 @@ import Login from "@/components/pages/Login"
 import { getServerSession } from "next-auth"
 import { authOptions } from "../api/auth/[...nextauth]/route"
 import { redirect } from "next/navigation"
-// import { Suspense } from 'react'
-//import Loading from "@/components/Loading"
 
 export default async function LoginPage(){
     const session = await getServerSession(authOptions)
--- frontend/components/Nav.jsx
+++ frontend/components/Nav.jsx
@@ -1,33 +1,86 @@
 import { IoClose } from "react-icons/io5";
 import { FaAngleDown, FaAngleUp } from "react-icons/fa";
 import Link from "next/link";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useUX } from "@/utils/context/ux/uxContext";
 
-export default function Nav(){
-    const [ subNavActive, setSubNavActive ] = useState(false)
-    const { state, dispatch } = useUX()
+export default function Nav() {
+    const [subNavActive, setSubNavActive] = useState(false);
+    const { state, dispatch } = useUX();
 
-    return(
+    useEffect(() => {
+        const body = document.body;
+
+        const handleClickOutside = (e) => {
+            const target = e.target;
+
+            if (state.isSideBarOpen && (target.tagName === 'A')) {
+                dispatch({ type: 'TOGGLE_SIDEBAR' });
+            }
+
+            if (state.isCartOpen || state.isModalOpen || state.isSideBarOpen) {
+                if (!target.closest('.cart') && !target.closest('.nav')) {
+                    if (state.isCartOpen) {
+                        dispatch({ type: 'TOGGLE_CART' });
+                    }
+                    if (state.isModalOpen) {
+                        dispatch({ type: 'TOGGLE_MODAL' });
+                    }
+                    if(state.isSideBarOpen){
+                        dispatch({type: 'TOGGLE_SIDEBAR'})
+                    }
+                }
+            }
+        };
+
+        body.addEventListener('click', handleClickOutside);
+
+        return () => {
+            body.removeEventListener('click', handleClickOutside);
+        };
+    }, [state.isSideBarOpen, state.isModalOpen, state.isCartOpen, dispatch]);
+
+
+    return (
         <nav className={`nav ${state.isSideBarOpen ? 'nav-active' : 'nav'}`}>
-            <div className="close-nav" onClick={()=> dispatch({type: 'TOGGLE_SIDEBAR'})}> <IoClose size={25}/> </div>
+            <div className="close-nav" onClick={() => dispatch({ type: 'TOGGLE_SIDEBAR' })}>
+                <IoClose size={25} className="close-icon" />
+            </div>
             <ul className="nav-list">
-                <li className="link"> <Link href='/'>Home</Link> </li>
+                <li className="link">
+                    <Link href='/'>Home</Link>
+                </li>
                 <ul className="link">
-                    <div className='shop-link' onClick={()=> setSubNavActive(!subNavActive)}>
-                        <span>Shop</span> { !subNavActive && <FaAngleDown /> } { subNavActive && <FaAngleUp /> }
+                    <div className='shop-link' onClick={() => setSubNavActive(!subNavActive)}>
+                        <span>Shop</span> 
+                        {!subNavActive && <FaAngleDown />} 
+                        {subNavActive && <FaAngleUp />}
                     </div>
                     <div className={`shop-link-mobile ${subNavActive ? 'shop-mobile-active' : 'shop-link-mobile'}`}>
-                        <li className="link"> <Link href='/shop/men/?category=men'>Men</Link> </li>
-                        <li className="link"> <Link href='/shop/women/?category=women'>Women</Link> </li>
-                        <li className="link"> <Link href='/shop/watches?category=wristwatch'>Watches</Link> </li>
-                        <li className="link"> <Link href='/shop/footwears/?category=footwear'>Footwears</Link> </li>
+                        <li className="link">
+                            <Link href='/shop/men/?category=men'>Men</Link>
+                        </li>
+                        <li className="link">
+                            <Link href='/shop/women/?category=women'>Women</Link>
+                        </li>
+                        <li className="link">
+                            <Link href='/shop/watches?category=wristwatch'>Watches</Link>
+                        </li>
+                        <li className="link">
+                            <Link href='/shop/footwears/?category=footwear'>Footwears</Link>
+                        </li>
                     </div>
                 </ul>
-                <li className="link"> <Link href='/our-story'>Our Story</Link> </li>
-                <li className="link"> <Link href='/blog'>Blog</Link> </li>
-                <li className="link"> <Link href='/contact-us'>Contact Us</Link> </li>
+                <li className="link">
+                    <Link href='/our-story'>Our Story</Link>
+                </li>
+                <li className="link">
+                    <Link href='/blog'>Blog</Link>
+                </li>
+                <li className="link">
+                    <Link href='/contact-us'>Contact Us</Link>
+                </li>
             </ul>
         </nav>
-    )
+    );
 }
