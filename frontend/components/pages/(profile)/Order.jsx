550f65e9abb874a5956dab422495d93b72a00e76
chineducode <chinedunnaemeka2@gmail.com>
2024-10-12 22:33:16 +0100

    order page modified

--- frontend/components/pages/(profile)/Order.jsx
+++ frontend/components/pages/(profile)/Order.jsx
@@ -10,21 +10,41 @@ import { useOrder } from "@/utils/context/order/orderContext"
 export default function Order(){
     const { data: session } = useSession()
     const { state, loadOrder } = useOrder()
+    const [search, setSearch] = useState('')
 
     useEffect(()=> {
         loadOrder()
         console.log(state.orders)
     },[])
 
+    const handleOrderFilter = (e) => {
+        setSearch(e.target.value)
+        console.log(search)
+    }
+
     return(
         <div className="order-page">
-            <div className="filter-container">
-                <input type="search" name="filter" id="" />
-            </div>
+            <form className="filter-form">
+                <input 
+                    type="search" 
+                    name="filter-order"
+                    value={search}
+                    onChange={handleOrderFilter}
+                    placeholder="search"
+                />
+                <IoSearch size={22}/>
+                <button type="submit">
+                    <span>Filter</span>
+                </button>
+            </form>
             {state?.orders?.length > 0 ?
-                <div className="orders-container">Your Orders</div>
+                <div className="orders-container">
+                    {state.orders.map((order, index) => (
+                        <div className="order">Hello Order</div>
+                    ))}
+                </div>
                 :
-                <div className="empty-orders">You have no orders yet</div>
+                <div className="empty-orders">You have no orders yet <br/>Continue shopping</div>
             }
         </div>
     )
